24430a02e1cf02cf082d5f4029028f8e9b2ebfe5
tyreecepaul <89tyreecepaul@gmail.com>
2025-10-02 11:07:34 +1000

    implemented utils.py (utility functions for training and using StyleGAN2

--- recognition/ADNI_StyleGAN_Diffusion_48837824/utils.py
+++ recognition/ADNI_StyleGAN_Diffusion_48837824/utils.py
@@ -1,17 +1,12 @@
 import torch
-from torch import nn, optim
-from torchvision import datasets, transforms
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
 from torchvision.utils import save_image
-from math import log2, sqrt
-import numpy as np
 import os
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+
+from modules import MappingNetwork
 
 """
-Util Functions
+utils.py
+Utility functions for training and using StyleGAN2
 """
 
 # Hyperparameters
@@ -25,65 +20,72 @@ Z_DIM = 256
 W_DIM = 256
 LAMBDA_GP = 10
 
+mapping_network = MappingNetwork(Z_DIM, W_DIM).to(DEVICE)
 
-def gradient_penalty(critic, real, fake,device="cpu"):
-    BATCH_SIZE, C, H, W = real.shape
-    beta = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
-    interpolated_images = real * beta + fake.detach() * (1 - beta)
-    interpolated_images.requires_grad_(True)
+def gradient_penalty(critic, real, fake, device=DEVICE):
+    batch_size, C, H, W = real.shape
+    alpha = torch.rand((batch_size, 1, 1, 1), device=device).expand_as(real)
+    interpolated = alpha * real + (1 - alpha) * fake.detach()
+    interpolated.requires_grad_(True)
 
-    # Calculate critic scores
-    mixed_scores = critic(interpolated_images)
- 
-    # Take the gradient of the scores with respect to the images
-    gradient = torch.autograd.grad(
-        inputs=interpolated_images,
+    mixed_scores = critic(interpolated)
+
+    grad_outputs = torch.ones_like(mixed_scores, device=device)
+    gradients = torch.autograd.grad(
         outputs=mixed_scores,
-        grad_outputs=torch.ones_like(mixed_scores),
+        inputs=interpolated,
+        grad_outputs=grad_outputs,
         create_graph=True,
         retain_graph=True,
     )[0]
-    gradient = gradient.view(gradient.shape[0], -1)
-    gradient_norm = gradient.norm(2, dim=1)
-    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
-    return gradient_penalty
+
+    gradients = gradients.view(batch_size, -1)
+    gradient_norm = gradients.norm(2, dim=1)
+    gp = ((gradient_norm - 1) ** 2).mean()
+    return gp
 
 
-def get_w(batch_size):
-    z = torch.randn(batch_size, W_DIM).to(DEVICE)
-    w = mapping_network(z)
-    return w[None, :, :].expand(LOG_RESOLUTION, -1, -1)
+def get_w(batch_size, mapping_network, device=DEVICE):
+    """
+    Sample z ~ N(0,1) and map to w using the mapping network.
+    Returns shape [LOG_RESOLUTION, batch_size, W_DIM]
+    """
+    z = torch.randn(batch_size, Z_DIM, device=device)
+    w = mapping_network(z)  # [batch_size, W_DIM]
+    # Expand along style layers for each resolution
+    w = w.unsqueeze(0).expand(LOG_RESOLUTION, -1, -1)
+    return w
 
 
-def get_noise(batch_size):
-        noise = []
-        resolution = 4
+def get_noise(batch_size, device=DEVICE):
+    """
+    Returns list of tuples (noise1, noise2) per resolution for StyleGAN2 generator.
+    noise1 can be None for the first block.
+    """
+    noise_list = []
+    resolution = 4
 
-        for i in range(LOG_RESOLUTION):
-            if i == 0:
-                n1 = None
-            else:
-                n1 = torch.randn(batch_size, 1, resolution, resolution, device=DEVICE)
-            n2 = torch.randn(batch_size, 1, resolution, resolution, device=DEVICE)
+    for i in range(LOG_RESOLUTION):
+        n1 = None if i == 0 else torch.randn(batch_size, 1, resolution, resolution, device=device)
+        n2 = torch.randn(batch_size, 1, resolution, resolution, device=device)
+        noise_list.append((n1, n2))
+        resolution *= 2
 
-            noise.append((n1, n2))
+    return noise_list
 
-            resolution *= 2
 
-        return noise
-
-def generate_examples(gen, epoch, n=100):
+def generate_examples(gen, mapping_network, epoch, n=100, device=DEVICE):
+    """
+    Generate 'n' images using the generator and save to saved_examples/epoch{epoch}/
+    """
     gen.eval()
-    alpha = 1.0
     for i in range(n):
         with torch.no_grad():
-            w = get_w(1)
-            noise = get_noise(1)
+            w = get_w(1, mapping_network, device=device)
+            noise = get_noise(1, device=device)
             img = gen(w, noise)
-            if not os.path.exists(f'saved_examples/epoch{epoch}'):
-                os.makedirs(f'saved_examples/epoch{epoch}')
-            save_image(img*0.5+0.5, f"saved_examples/epoch{epoch}/img_{i}.png")
-
+            folder = f'saved_examples/epoch{epoch}'
+            os.makedirs(folder, exist_ok=True)
+            save_image(img * 0.5 + 0.5, f"{folder}/img_{i}.png")
     gen.train()
-
 
